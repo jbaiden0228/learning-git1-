f02be41399213750f5406ea0bc54414fb4a61a93
jbaiden0228 <129003986+jbaiden0228@users.noreply.github.com>
2023-04-09 16:36:06 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,12 @@
 I am glad to be here today
 Today is Easter Sunday
+5.8k
+Mike Bebernes
+Mike Bebernes·Senior Editor
+Sun, April 9, 2023 at 9:45 AM EDT
+“The 360” shows you diverse perspectives on the day’s top stories and debates.
+
+What’s happening
+On Tuesday night, while most of the political world was still focusing on the indictment of former President Donald Trump, a liberal candidate secured a major win that arguably suggests more about how future national elections may go than anything that happened in that New York City courtroom earlier in the day.
+
+In Wisconsin, a liberal judge, Janet Protasiewicz, decisively defeated her conservative opponent, Daniel Kelly, and secured a seat on the state’s Supreme Court in a race widely considered to be the most important election of 2023. Protasiewicz’s victory will give liberals a majority on the Wisconsin court for the first time in 15 years. This potentially offers them
