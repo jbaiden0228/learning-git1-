8ce5f29fa06ee5a0c8171e03d96e5e8913719259
jonathan baiden <jbaiden0228@gmail.com>
2023-04-09 16:44:52 -0400

    Added html script

--- main.go
+++ main.go
@@ -10,3 +10,16 @@ What’s happening
 On Tuesday night, while most of the political world was still focusing on the indictment of former President Donald Trump, a liberal candidate secured a major win that arguably suggests more about how future national elections may go than anything that happened in that New York City courtroom earlier in the day.
 
 In Wisconsin, a liberal judge, Janet Protasiewicz, decisively defeated her conservative opponent, Daniel Kelly, and secured a seat on the state’s Supreme Court in a race widely considered to be the most important election of 2023. Protasiewicz’s victory will give liberals a majority on the Wisconsin court for the first time in 15 years. This potentially offers them
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
