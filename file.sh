1cffae1697dce18533778c56d769896afa838478
Tonybea <129562365+Tonybea@users.noreply.github.com>
2023-04-23 21:03:06 +0000

    pythong script

--- file.sh
+++ file.sh
@@ -7,3 +7,7 @@ thistuple = ("apple")
 print(type(thistuple))
 <p 
 ="About W3Schools">W3Schools is a web developer's site.</p>
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
