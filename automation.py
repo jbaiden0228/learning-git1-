f6e9530e427958633270deaffed3da227779bdac
Tonybea <129562365+Tonybea@users.noreply.github.com>
2023-04-23 22:45:57 +0000

    Update automation.py
    added pythong scipt

--- automation.py
+++ automation.py
@@ -16,3 +16,19 @@ mycursor.execute(sql)
 mydb.commit()
 
 print(mycursor.rowcount, "record(s) affected")
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+  
+  class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
