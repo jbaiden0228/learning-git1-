e176aaa2df8145701bf872b51e713ca1cb143c2d
SammiBe <129970198+SammiBe@users.noreply.github.com>
2023-04-23 20:21:02 +0000

    Create file.sh

--- file.sh
+++ file.sh
@@ -1,13 +1,2 @@
 1234567890
-thistuple = ("apple",)
-print(type(thistuple))
-
-#NOT a tuple
-thistuple = ("apple")
-print(type(thistuple))
-print("Guru99")
-
-#!/bin/bash
-while IFS= read -r line; do
-   echo "$line"
-done <filename.txt
+print("Hello, World!")print("Hello, World!")
