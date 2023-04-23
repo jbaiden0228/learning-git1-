fc6262d3d25125027d939856b30612b15c4c4366
atsu12 <70347176+atsu12@users.noreply.github.com>
2023-04-23 21:03:14 +0000

    frank's second cmmi

--- file.sh
+++ file.sh
@@ -2,8 +2,4 @@
 thistuple = ("apple",)
 print(type(thistuple))
 
-#NOT a tuple
-thistuple = ("apple")
-print(type(thistuple))
-<p 
-="About W3Schools">W3Schools is a web developer's site.</p>
+#NOT a
