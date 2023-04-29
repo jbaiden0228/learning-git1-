334cdede2f6b65e4afc14eec5c7464bb496dc622
jbaiden0228 <129003986+jbaiden0228@users.noreply.github.com>
2023-04-29 12:47:47 -0400

    Update automation.py

--- automation.py
+++ automation.py
@@ -6,29 +6,19 @@ mydb = mysql.connector.connect(
   password="yourpassword",
   database="mydatabase"
 )
-
 mycursor = mydb.cursor()
-
 sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
-
 mycursor.execute(sql)
-
 mydb.commit()
-
 print(mycursor.rowcount, "record(s) affected")
-
-
 def myFunction() :
   return True
-
 if myFunction():
   print("YES!")
 else:
   print("NO!")
-  
   class myclass():
   def __len__(self):
     return 0
-
 myobj = myclass()
 print(bool(myobj))
